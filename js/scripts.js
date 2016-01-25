db67e47861b55800fcd5ef2b3239233979fcfc08
Jason Awbrey and Will Johnson <trashmoldwilliams@gmail.com>
2016-01-25 11:29:19 -0800

    Add strikethrough function

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,17 @@ var Task = function (name, description, dueDate, priority, status){
 Task.prototype.completeTask = function () {
   return this.status = "Complete";
 };
+
+$(function() {
+  $('form#createTask').submit(function(event) {
+    event.preventDefault();
+    var taskEntry = $('input#taskEntry').val();
+    var currentTask = new Task (taskEntry);
+    $('ul#toDoList').append("<li><span class='current'>" + currentTask.name + "</span></li>");
+
+    $('.current').last().click(function(){
+      $(this).addClass("strikeThrough");
+    });
+
+  });
+});
